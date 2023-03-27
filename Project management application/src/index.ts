522b76f656b1f07dacf9b79b74f2ede41fbb02bc
azizbekkhabibullaev <azizbekkhabibullaev@mail.ru>
2023-03-27 05:57:10 +0500

    changes in claster src index

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -1,23 +1,22 @@
-import mongoose from 'mongoose';
-import { PORT } from './constants';
+import mongoose from "mongoose";
+import { PORT } from "./constants";
 
-import * as serverService from './services/server.service';
-
+import * as serverService from "./services/server.service";
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect(
+      "mongodb+srv://anonymous:anonymous@cluster0.9cihe0w.mongodb.net/?retryWrites=true&w=majority"
+    );
     serverService.server.listen(process.env.PORT || PORT, function () {
-      console.log('Сервер ожидает подключения...');
-    })
+      console.log("Сервер ожидает подключения...");
+    });
   } catch (error) {
     console.log(error);
   }
 })();
 
-
-
-process.on('SIGINT', async () => {
+process.on("SIGINT", async () => {
   await mongoose.disconnect();
   process.exit();
 });
